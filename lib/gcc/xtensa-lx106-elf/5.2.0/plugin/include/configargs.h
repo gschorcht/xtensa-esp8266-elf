104e40f257eb73c95efdf26fd8d50d5fbd725522
Gunar Schorcht <gunar@schorcht.net>
2019-03-14 10:18:19 +0100

    initial toolchain commit

---
+++ lib/gcc/xtensa-lx106-elf/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/gs/esp/crosstool-NG/.build/src/gcc-5.2.0/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=xtensa-lx106-elf --prefix=/home/gs/esp/crosstool-NG/builds/xtensa-lx106-elf --with-local-prefix=/home/gs/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --with-sysroot=/home/gs/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG crosstool-ng-1.22.0-80-g6c4433a5' --disable-__cxa_atexit --enable-cxx-flags='-fno-exceptions -fno-rtti' --with-gmp=/home/gs/esp/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-mpfr=/home/gs/esp/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-mpc=/home/gs/esp/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-isl=/home/gs/esp/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-cloog=/home/gs/esp/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-libelf=/home/gs/esp/crosstool-NG/.build/xtensa-lx106-elf/buildtools --enable-lto --enable-target-optspace --without-long-double-128 --disable-libgomp --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-nls --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
